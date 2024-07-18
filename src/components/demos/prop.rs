5d0c2bc720bb4210cb93f4a9547426fd02e34a89
zwpdbh <hyperion_z@outlook.com>
2024-07-18 17:00:24 +0800

    > build form demo

--- src/components/demos/prop.rs
+++ src/components/demos/prop.rs
@@ -9,6 +9,69 @@ pub fn DemoProp() -> Element {
             li {
                 SimpleProp { score: 42 }
             }
+            li { GeneralCard { 
+                h1 { "Prop Option" }
+                ul {
+                    li { GeneralCard { 
+                        PropOtions { title: "Some Title" }
+                    } }
+
+                    li { GeneralCard { 
+                        PropOtions { title: "Some Title", subtitle: "Some Subtitle" }
+                    } }
+                }
+            } }
+            li { GeneralCard { 
+                h1 { "Explicitly Required Option" }
+                ul {
+                    li { GeneralCard { 
+                        ExplicitOption { title: "Some Title", subtitle: None }
+                    } }
+
+                    li { GeneralCard { 
+                        ExplicitOption { title: "Some Title", subtitle: Some("Some SubTitle".to_string()) }
+                    } }
+                }
+            } }
+
+            li { GeneralCard { 
+                h1 { "Default Props" }
+
+                ul {
+                    li { GeneralCard { 
+                        DefaultComponent { number: 5 }
+                    } }
+
+                    li { GeneralCard { DefaultComponent {} } }
+                }
+            } }
+
+            li { GeneralCard { 
+                h1 { "Automatic Conversion with into" }
+                IntoComponent { string: "some &str can be accepted instead of just String" }
+            } }
+
+            li { GeneralCard { 
+                h1 { "ComponentChildrenV1" }
+                ComponentChildrenV1 {
+                    href: "https://www.youtube.com/watch?v=C-M2hs3sXGo",
+                    body: rsx! {
+                        "How to "
+                        i { "not" }
+                        " be seen"
+                    }
+                }
+            } }
+
+            li { GeneralCard { 
+                h1 { "ComponentChildrenV2 pass element like children instead of pass rsx!" }
+                p { "This is must be done from the `magic` children prop" }
+                ComponentChildrenV2 { href: "https://www.youtube.com/watch?v=C-M2hs3sXGo",
+                    "How to "
+                    i { "not" }
+                    " be seen"
+                }
+            } }
         }
     )
 }
@@ -30,4 +93,86 @@ fn SimpleProp(props: LikesProps) -> Element {
             }
         }
     )
+}
+
+#[derive(PartialEq, Clone, Props)]
+struct OptionalProps {
+    title: String,
+    subtitle: Option<String>,
+}
+
+#[component]
+fn PropOtions(props: OptionalProps) -> Element {
+    rsx! {
+        p {
+            "{props.title}: "
+            {props.subtitle.unwrap_or_else(|| "No subtitle provided".to_string())}
+        }
+    }
+}
+
+#[derive(PartialEq, Clone, Props)]
+struct ExplicitOptionProps {
+    title: String,
+    #[props(!optional)]
+    subtitle: Option<String>,
+}
+
+fn ExplicitOption(props: ExplicitOptionProps) -> Element {
+    rsx! {
+        p {
+            "{props.title}: "
+            {props.subtitle.unwrap_or_else(|| "No subtitle provided".to_string())}
+        }
+    }
+}
+
+#[derive(PartialEq, Props, Clone)]
+struct DefaultProps {
+    // default to 42 when not provided
+    #[props(default = 42)]
+    number: i64,
+}
+
+fn DefaultComponent(props: DefaultProps) -> Element {
+    rsx! {
+        p { "{props.number}" }
+    }
+}
+
+#[derive(PartialEq, Props, Clone)]
+struct IntoProps {
+    #[props(into)]
+    string: String,
+}
+
+fn IntoComponent(props: IntoProps) -> Element {
+    rsx! {
+        p { "{props.string}" }
+    }
+}
+
+#[derive(PartialEq, Clone, Props)]
+struct ClickableProps {
+    href: String,
+    body: Element,
+}
+
+fn ComponentChildrenV1(props: ClickableProps) -> Element {
+    rsx! {
+        a { href: "{props.href}", class: "fancy-button", {props.body} }
+    }
+}
+
+#[derive(PartialEq, Clone, Props)]
+struct ClickablePropsV2 {
+    href: String,
+    // The "magic" children prop
+    children: Element,
+}
+
+fn ComponentChildrenV2(props: ClickablePropsV2) -> Element {
+    rsx! {
+        a { href: "{props.href}", class: "fancy-button", {props.children} }
+    }
 }
