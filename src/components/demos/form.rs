211eba52b3ffc060bb58b2faba854eceba55d89f
zwpdbh <hyperion_z@outlook.com>
2024-07-19 16:56:47 +0800

    > simulate the layout and form from dioxus openai qa gui

--- src/components/demos/form.rs
+++ src/components/demos/form.rs
@@ -1,47 +1,8 @@
 #![allow(non_snake_case)]
 use dioxus::prelude::*;
 
-#[allow(unused)]
-#[derive(Debug)]
-enum FormDemoSubject {
-    Option01,
-    Option02,
-}
-
-#[derive(Debug)]
-struct FormDemoData {
-    name: String,
-    username: String,
-    email: String,
-    subject: FormDemoSubject,
-    message: String,
-    agree: bool,
-}
-
+/// Example from: https://github.com/fairjm/dioxus-openai-qa-gui
 #[component]
 pub fn Form() -> Element {
-    let user_input: Option<FormDemoData> = None;
-
-    match user_input {
-        Some(input) => {
-            rsx!(
-                div {
-                    h1 { "Form Data" }
-                    p { "Name: {input.name}" }
-                    p { "Username: {input.username}" }
-                    p { "Email: {input.email}" }
-                    p { "Subject: {input.subject:?}" }
-                    p { "Message: {input.message}" }
-                    p { "Agree: {input.agree}" }
-                }
-            )
-        }
-        None => {
-            rsx!(
-                div {
-                    h1 { "No Data Available" }
-                }
-            )
-        }
-    }
+    rsx!()
 }
